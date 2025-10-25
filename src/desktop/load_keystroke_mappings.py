c40d884dbbd362be31e6cb92d0da6c7fa3c88be4
raymondlin336 <94279301+raymondlin336@users.noreply.github.com>
2025-10-25 18:54:37 -0400

    Prioritize loading user config

--- src/desktop/load_keystroke_mappings.py
+++ src/desktop/load_keystroke_mappings.py
@@ -1,12 +1,18 @@
 import json
 
-def load_keystrokes(json_path="Settings/default_mappings.json"):
-
+def load_keystrokes():
+    default_path = "Settings/default_mappings.json"
+    user_path = "Settings/user_mappings.json"
     try:
-        with open(json_path, 'r') as file:
+        with open(user_path, 'r') as file:
             data = json.load(file)
         return data
     except FileNotFoundError:
-        print("Error: 'data.json' not found.")
+        try:
+            with open(default_path, 'r') as file:
+                data = json.load(file)
+            return data
+        except FileNotFoundError:
+            print("Neither JSON files were found.")
     except json.JSONDecodeError:
         print("Error: Invalid JSON format in 'data.json'.")
