98f15dc27a1ce7d36b46f67e051955cfd56d839e
Adrian Yip <yipadrian1119@gmail.com>
2025-10-25 13:53:19 -0400

    Using pynput library instead of pyautoGUI library for cursor

--- src/cursor_control.py
+++ src/cursor_control.py
@@ -1,17 +1,35 @@
-import pyautogui
+from pynput.mouse import Controller
+import subprocess
+
+
+def get_screen_size():
+    try:
+        result = subprocess.run(['xrandr'], capture_output=True, text=True)
+        lines = result.stdout.split('\n')
+        for line in lines:
+            if '*' in line:
+                resolution = line.split()[0]
+                width, height = map(int, resolution.split('x'))
+                return width, height
+    except:
+        pass
+    return 1920, 1080
 
 def move_cursor(webcam_x, webcam_y):
     """
-    Simple cursor control - maps webcam to screen
+    Simple cursor control - maps webcam to screen using pynput
     
     Args:
         webcam_x (int): X coordinate in webcam (0-640)
         webcam_y (int): Y coordinate in webcam (0-480)
     """
 
-    screen_width, screen_height = pyautogui.size()
+    screen_width, screen_height = get_screen_size()
+    
 
     screen_x = int((webcam_x / 640) * screen_width)
     screen_y = int((webcam_y / 480) * screen_height)
+    
 
-    pyautogui.moveTo(screen_x, screen_y)
+    mouse = Controller()
+    mouse.position = (screen_x, screen_y)
--- src/index_mouse.py
+++ src/index_mouse.py
@@ -3,7 +3,3 @@ import mediapipe as mp
 import get_hand
 from src.get_hand import hands
 from src.get_webcam_open import landmarks
-
-if hands:
-    for hand in hands:
-        for id in
